6df026feee36e32526f7eecb4b2619cb8e92046c
Michael Ivanchenko <misha@kvvaiu.ru>
2019-01-21 22:59:44 +0300

    2019-01-21 initial commit

---
+++ Lesson01WebApp/src/main/java/ru/imv/HelloServlet.java
@@ -0,0 +1,12 @@
+package ru.imv;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
